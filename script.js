0baedb0f7aced494ff88a2257188fcd4adfca94a
Williamfox2012 <141017859+Williamfox2012@users.noreply.github.com>
2024-02-08 20:35:35 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.container').on('mousedown', function(e) {
+        $(this).css('transform', 'translateY(200px)'); /* انتقال صفحه به پایین با 200 پیکسل */
+    });
+});
