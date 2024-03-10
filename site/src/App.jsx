399dde3973eb8fb06fa52419183b9efbb8a72f56
Williamfox2012 <141017859+Williamfox2012@users.noreply.github.com>
2024-03-10 19:58:54 +0330

    first commit

---
+++ site/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Head from "./components/header";
+
+function App() {
+  return (
+    <>
+      <Head />
+    </>
+  );
+}
+
+export default App;
---
+++ site/src/components/header.jsx
@@ -0,0 +1,7 @@
+export default function Head() {
+  return (
+    <div className="header">
+      <h1 id="pr">programming month</h1>
+    </div>
+  );
+}
